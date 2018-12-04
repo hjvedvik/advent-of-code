b8a2e91c980745bf785398ba08dde0b38cc22f5a
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-12-05 00:29:43 +0100

    update: day 4 - repose record

--- 2018/04-repose-record/index.js
+++ 2018/04-repose-record/index.js
@@ -1,39 +1,37 @@
 const lines = require('fs').readFileSync('input.txt', 'utf8').trim().split('\n')
-const records = lines.map(v => v.match(/\[.*\]\s(?:\w+\s#\d+\s)?(.*)/)).sort()
+const records = lines.sort().map(v => v.match(/\[.*\]\s(?:\w+\s#\d+\s)?(.*)/))
 const mins = new Array(60).fill(0)
 const index = {}
 
-let current, sleeping
+let i, num, current, sleeping
 
 for (const [ raw, msg ] of records) {
-  const num = +raw.match(/\d+/g).pop()
+  num = +raw.match(/\d+/g).pop()
 
   if (msg === 'begins shift')
     current = index[num] = (index[num] || { id: num, mins: [...mins] })
   
   if (msg === 'wakes up')
-    for (let i = sleeping; i < num; i++) current.mins[i]++
+    for (i = sleeping; i < num; i++) current.mins[i]++
   
   if (msg === 'falls asleep')
     sleeping = num
 }
 
 function part1 () {
-  const guard = Object.values(index)
+  return Object.values(index)
     .map(guard => ({ ...guard, total: guard.mins.reduce((a, b) => a + b, 0) }))
-    .sort((a, b) => b.total - a.total).shift()
-  
-  return guard.id * guard.mins.indexOf(Math.max(...guard.mins))
+    .sort((a, b) => b.total - a.total).slice(0, 1)
+    .reduce((a, { id, mins }) => id * mins.indexOf(Math.max(...mins)), 0)
 }
 
 console.log('part 1:', part1()) // 26281
 
 function part2 () {
-  const guard = Object.values(index)
+  return Object.values(index)
     .map(guard => ({ ...guard, max: Math.max(...guard.mins) }))
-    .sort((a, b) => b.max - a.max).shift()
-
-  return guard.id * guard.mins.indexOf(guard.max)
+    .sort((a, b) => b.max - a.max).slice(0, 1)
+    .reduce((a, { id, mins, max }) => id * mins.indexOf(max), 0)
 }
 
 console.log('part 2:', part2()) // 73001
