95f72a6fae89af9128e428556f776c4d3759297c
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2018-12-05 23:50:29 +0100

    update day 5 for speed

--- 2018/05-alchemical-reduction/index.js
+++ 2018/05-alchemical-reduction/index.js
@@ -1,26 +1,28 @@
 const input = require('fs').readFileSync('input.txt', 'utf8').trim()
+const units = input.split('').map(c => [c.charCodeAt(0), c.toLowerCase().charCodeAt(0)])
+const reacts = (a, b) => a && Math.abs(a[0] - b[0]) === 32
 
-function react (str) {
-  for (let i = 0, l = str.length - 1; i < l; i++) {
-    if (i < 0 || str[i] === str[i + 1]) continue
-    if (str[i].toUpperCase() === str[i + 1] || str[i] === str[i + 1].toUpperCase())
-      str = str.slice(0, i) + str.slice(i + 2), i -= 2, l -= 2
+function scan (units, skip) {
+  const stack = []
+
+  for (const unit of units) {
+    if (unit[1] === skip) continue
+    else if (reacts(stack[stack.length - 1], unit)) stack.pop()
+    else stack.push(unit)
   }
 
-  return str
+  return stack.length
 }
 
 function part1 () {
-  return react(input).length
+  return scan(units)
 }
 
 console.log('part 1:', part1()) // 11108
 
 function part2 () {
-  return [...Array(26).keys()].map(n => (n + 10).toString(36))
-    .map(c => new RegExp(`${c}|${c.toUpperCase()}`, 'g'))
-    .map(r => input.replace(r, ''))
-    .map(s => react(s).length)
+  return [...Array(26).keys()].map(n => (n + 10).toString(36).charCodeAt(0))
+    .map(c => scan(units, c))
     .sort((a, b) => a - b)
     .shift()
 }
